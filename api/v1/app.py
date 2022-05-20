a8098c95d608f5dc92fff835704852b565b6e0fa
Luis Colon <luiscolon0426@gmail.com>
2022-05-20 15:19:01 -0400

    made chmod

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,10 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.teardown_appcontext
+def close():
+    storage.close()
+
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST")
     if not host:
