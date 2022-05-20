09402c2b41e7c774ba4acf1a10e5a254187a8831
Luis Colon <luiscolon0426@gmail.com>
2022-05-20 16:55:34 -0400

    pls

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
 ''' module that makes this an app '''
 
-from os import getenv
-from flask import Flask
+from flask import Flask, abort, jsonify
 from models import storage
 from api.v1.views import app_views
-
-
+from os import getenv
 app = Flask(__name__)
+
+
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close():
+def close(self):
     ''' method that close the yes '''
     storage.close()
 
 
-if __name__ == "__main__":
-    ''' only executes as main '''
-    host = getenv("HBNB_API_HOST")
-    if not host:
-        host = "0.0.0.0"
-    port = getenv("HBNB_API_PORT")
-    if not port:
-        port = "5000"
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
