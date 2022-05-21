db277ee6acb432d850d52bc23c84ef30c13ea102
Luis Colon <luiscolon0426@gmail.com>
2022-05-21 08:58:13 -0400

    Added task 5, creating an endpoint to each object

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,13 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.user import User
+from models.review import Review
 
 
 @app_views.route('/status')
@@ -10,3 +17,16 @@ def get_status():
     ''' route status test that return a JSON query '''
     dict = {'status': 'OK'}
     return jsonify(dict)
+
+
+@app_views.route('/status')
+def newcount():
+    data = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "place": storage.count(Place),
+        "user": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify
