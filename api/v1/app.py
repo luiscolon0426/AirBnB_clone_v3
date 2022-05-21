c3e70b9ba98c06e5970f378b90bcb74d965aebf1
Luis Colon <luiscolon0426@gmail.com>
2022-05-21 11:26:07 -0400

    Added 404 from task 6 / task 4 still has some errors with the port 5000 connection

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,11 @@ def close(self):
     storage.close()
 
 
+@app.errorhandler(404)
+def invalid_route(e):
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == '__main__':
     app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
             port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
