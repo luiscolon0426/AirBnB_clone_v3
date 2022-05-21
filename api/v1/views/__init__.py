506cf7d71da1cf541d487fe3fa7bcd349ed2cc4f
Luis Colon <luiscolon0426@gmail.com>
2022-05-21 15:09:41 -0400

    fixed import order

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,9 +3,9 @@
 init file
 """
 
-import api.v1.views.amenities
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 import api.v1.views.states
 import api.v1.views.cities
+import api.v1.views.amenities
